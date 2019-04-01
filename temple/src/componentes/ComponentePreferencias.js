bcffd4e7c00517148ad98833b2305c05d0351c46
Gricardov <gricardov@gmail.com>
2019-03-31 23:48:08 -0500

    fetch sugerencias agregado v.1

--- temple/src/componentes/ComponentePreferencias.js
+++ temple/src/componentes/ComponentePreferencias.js
@@ -1,59 +1,87 @@
 import React, { Component } from 'react';
 import { Card, CardBody, Label, Col, FormGroup } from 'reactstrap';
 import { Control } from 'react-redux-form';
+import { URLBase } from '../compartido/URLBase';
 
 class Preferencias extends Component {
 
     constructor(props) {
 
         super(props);
+        this.state = {
+            consulta: '',
+            resultados: []
+        }
+        this.consultar = this.consultar.bind(this);
+
+    }
+
+    consultar(e) {
+
+        return fetch(URLBase + 'registro/consultarCurso/' + e.target.value)
+            .then(response => {
+
+                if (response.ok) {
+
+                    return response;
+
+                }
+
+                else {
+
+                    var error = new Error("Ha ocurrido un error con el siguiente mensaje:\n" + response.status + " : " + response.statusText);
+                    error.response = response;
+                    throw error;
+
+                }
+
+
+            }, error => {
+
+                var mensErr = new Error(error.message);
+                throw mensErr;
+
+            })
+            .then(response => response.json())
+            .then(res => {
+                this.setState({ resultados: res })
+            })
+            .catch(error => {
+                console.log("Error : " + error.message)
+
+            })
 
     }
 
     render() {
         //const preferencias = [...Array(this.props.numPreferencias)].map((_, i) => {
-            const preferencias=this.props.preferencias.map((e,i)=>{
+        const preferencias = this.props.preferencias.map((e, i) => {
             return (
                 <Card key={i} className="mb-3">
                     <CardBody>
                         <FormGroup row>
-                            { i>0 ? 
-                            <Label xs={12}>¿Qué otro curso deseas priorizar?</Label>
-                            :
-                            <Label xs={12}>¿Qué cursos priorizamos para ti?<small>
-                                Puedes elegir otros cursos luego</small></Label>
+                            {i > 0 ?
+                                <Label xs={12}>¿Qué otro curso deseas priorizar?</Label>
+                                :
+                                <Label xs={12}>¿Qué cursos priorizamos para ti? <small>
+                                    Puedes elegir otros cursos luego</small></Label>
                             }
-                            
-                            <Col xs={12} md={6}>
-                                <Label htmlFor="cboCategoria">Categoria: </Label>
-                                <Control.select
+
+                            <Col xs={12}>
+                                <Control.text
                                     className="form-control"
-                                    model=".categoria"
-                                    id="cboCategoria"
-                                    name="categoria"
-                                >
-                                    <option>Ingenieria</option>
-                                    <option>Ciencias sociales</option>
-                                    <option>Ciencias de la salud</option>
-                                    <option>Psicología</option>
-                                </Control.select>
+                                    model=".curso"
+                                    id="txtCurso"
+                                    name="curso"
+                                    placeholder="Por ejemplo: Java, SQL Server, MySQL"
+                                    autoComplete="off"
+                                    onChange={(e) => this.consultar(e)}
+                                />
+                                <Col xs={12}>
+                                <Sugerencias resultados={this.state.resultados}/>
+                                </Col>
                             </Col>
-                            <Col xs={12} md={6}>
-                                <Label htmlFor="cboSubcategoria">Subcategoria: </Label>
-                                <Control.select
-                                    className="form-control"
-                                    model=".subcategoria"
-                                    id="cboSubcategoria"
-                                    name="subcategoria"
-                                >
-                                    <option>Cálculo</option>
-                                    <option>Física</option>
-                                    <option>Química</option>
-                                    <option>Estadística</option>
-                                    <option>Asesoría de tesis</option>
-                                    <option>Programación</option>
-                                </Control.select>
-                            </Col>
+
                         </FormGroup>
 
                     </CardBody>
@@ -70,5 +98,13 @@ class Preferencias extends Component {
 
 }
 
+const Sugerencias = (props) => {
+    const options = props.resultados.map((e,i)=> (
+        <div key={e.ID_CUR}>
+           <strong>{e.NOM_CUR}</strong>
+        </div>
+    ))
+    return <div className="autocomplete-items">{options}</div>
+}
 
 export default Preferencias;
