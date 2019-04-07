e555cd8499ebc736d6cfa33a9d85647a7cbf6c42
Gricardov <gricardov@gmail.com>
2019-04-07 16:31:28 -0500

    cambio componente preferencias

--- temple/src/componentes/ComponentePreferencias.js
+++ temple/src/componentes/ComponentePreferencias.js
@@ -92,6 +92,9 @@ class Preferencias extends Component {
     }
 
     render() {
+
+        console.log(JSON.stringify(this.props.preferencias))
+
         //const preferencias = [...Array(this.props.numPreferencias)].map((_, i) => {
         const preferencias = this.props.preferencias.map((e, i) => {
             //console.log(JSON.stringify(this.state))
@@ -184,9 +187,9 @@ const Sugerencias = (props) => {
             <strong>{e.NOM_CUR}</strong> - Perteneciente a la categoría Ciencias de la computación
             <br />
             Relacionados:{' '}
-            <span class="badge badge-pill badge-primary">ruby</span>{' '}
-            <span class="badge badge-pill badge-success">sql</span>{' '}
-            <span class="badge badge-pill badge-info">sarita</span>
+            <span className="badge badge-pill badge-primary">ruby</span>{' '}
+            <span className="badge badge-pill badge-success">sql</span>{' '}
+            <span className="badge badge-pill badge-info">sarita</span>
         </div>
     ))
     return <div className="autocomplete-items">{options}</div>
