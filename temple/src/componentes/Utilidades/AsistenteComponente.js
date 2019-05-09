b82ebde564b5b4b4603fb89adb45a349bb11f511
unknown <SUITE@SUITESM701-ST01.cibertec.edu.pe>
2019-05-08 21:06:14 -0500

    chatbox 0.1

---
+++ temple/src/componentes/Utilidades/AsistenteComponente.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Asistente extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+
+        };
+    }
+
+    render(){
+        return(
+            null
+        )
+    }
+
+}
+
+export default Asistente;
