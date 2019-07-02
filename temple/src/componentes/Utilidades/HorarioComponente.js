1c2d26dbd08a8faa196793d5f4ff2293ace51c34
gricardov <gricardov@gmail.com>
2019-07-02 03:19:41 -0500

    horarios semi ok

--- temple/src/componentes/Utilidades/HorarioComponente.js
+++ temple/src/componentes/Utilidades/HorarioComponente.js
@@ -5,30 +5,34 @@ import { ReactAgenda, ReactAgendaCtrl, guid, Modal } from 'react-agenda';
 require('moment/locale/es.js');
 let moment = require('moment');
 
-let colors = {
-  'color-1': "rgba(102, 195, 131 , 1)",
-  "color-2": "rgba(242, 177, 52, 1)",
-  "color-3": "rgba(235, 85, 59, 1)"
+let colorEvento = {
+  'color-1':"rgb(61, 255, 142)",
+  "color-2":"rgb(51, 129, 255)"
+}
+
+let colorSeleccionado = {
+  "color-2":"rgb(51, 129, 255)"
 }
 
 // Le quito 5 horas porque estoy en UTC -5
 let now = new Date();
-now.setHours( now.getHours() - 5 );
+now.setHours(now.getHours() - 5);
 
 class Horario extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selected: [],
-      cellHeight: 30,
+      cellHeight: 40,
       showModal: false,
       locale: "es",
-      rowsPerHour: 6,
+      rowsPerHour: 4,
       numberOfDays: 7,
       startDate: now,
       eventos: []
     }
-
+    this.agregarEvento = this.agregarEvento.bind(this)
+    this.editarEvento=this.editarEvento.bind(this)
     this.editarItem = this.editarItem.bind(this)
     this.seleccionarRango = this.seleccionarRango.bind(this)
     this.seleccionarCelda = this.seleccionarCelda.bind(this)
@@ -36,81 +40,95 @@ class Horario extends Component {
     this.modificarEvento = this.modificarEvento.bind(this)
     this.removerItem = this.removerItem.bind(this)
     this.cerrarModal = this.cerrarModal.bind(this)
+    this.abrirModal = this.abrirModal.bind(this)
+
+  }
+  agregarEvento (items , newItems){
+
+    let eventos=[];
+    eventos=this.state.eventos;
+    // Le debes quitar 5 horas
+    this.setState({showModal:false ,selected:[] , eventos: [...eventos,...items]});
+    this.cerrarModal();
   }
 
-  seleccionarRango(item) {
-    console.log('handleCellSelection', item)
+  editarEvento (items , item){
+    this.setState({showModal:false ,selected:[] , eventos:items});
+    this.cerrarModal();
   }
-  editarItem() {
+  seleccionarRango(selected) {
+    //alert(selected)
+    this.setState({ selected: selected, showCtrl: true })
+    this.abrirModal();
+  }
+  editarItem(item,openModal) {
+    if (item && openModal === true) {
+      this.setState({ selected: [item] })
+      return this.abrirModal();
+    }
+  }
+  seleccionarCelda(item, openModal) {
+    if (this.state.selected && this.state.selected[0] === item) {
+      return this.abrirModal();
+    }
+    this.setState({ selected: [item] })
+  }
+  cambiarDuracionEvento(items, item) {
+    this.setState({ eventos: items })
+  }
+  modificarEvento(items, item) {
+    this.setState({ eventos: items })
+  }
+  removerItem(items, item) {
+    this.setState({ eventos: items });
+  }
+
+  abrirModal() {
     this.setState({
       showModal: true
     })
   }
-  seleccionarCelda(item) {
 
+  cerrarModal(e) {
+    if (e) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    this.setState({ showModal: false })
   }
-  cambiarDuracionEvento(item) {
 
-  }
-  modificarEvento(item) {
+  componentDidMount() {
+    // Transformo los eventos al formato requerido
+    let eventos = [];
+    if (this.props.eventos) {
+         this.props.eventos.map((e, i) => {
 
-  }
-  removerItem(item) {
+        let momentoInicio = moment(e.fecIni);
+        let momentoFin = moment(e.fecFin);
 
-  }
-  cerrarModal() {
+        let fechaIni = new Date(momentoInicio.year(), momentoInicio.month(), momentoInicio.day(), momentoInicio.hour(), momentoInicio.minute(), momentoInicio.second());
+        let fechaFin = new Date(momentoFin.year(), momentoFin.month(), momentoFin.day(), momentoFin.hour(), momentoFin.minute(), momentoFin.second());
+        eventos.push({
+          _id: e.idHor,
+          name: e.tipo,
+          startDateTime: fechaIni,
+          endDateTime: fechaFin,
+          classes: 'color-1'
+        })
+
+        console.log(fechaIni + " " + fechaFin)
+      })
+    }
+
+    // Y lo establezco como el estado
     this.setState({
-      showModal: false
+      eventos: eventos
     })
-  }
-
-  componentDidMount(){
-// Transformo los eventos al formato requerido
-let eventos = [];
-if (this.props.eventos) {
-  /*[{
-    _id            :guid(),
-     name          : 'Meeting , dev staff!',
-     startDateTime : new Date(2019,6,1,15,33,33),
-     endDateTime   : new Date(2019,6,1,16,33,33),
-     classes       : 'color-1 color-4'
-   },
-   {
-    _id            :guid(),
-     name          : 'Working lunch , Holly',
-     startDateTime : new Date(2019,6,2,17,33,33),
-     endDateTime   : new Date(2019,6,2,18,33,33),
-     classes       : 'color-2'
-   }]*/
-   this.props.eventos.map((e, i) => {
-
-    let momentoInicio=moment(e.fecIni);
-    let momentoFin=moment(e.fecFin);
-
-    let fechaIni=new Date(momentoInicio.year(), momentoInicio.month(), momentoInicio.day(), momentoInicio.hour(), momentoInicio.minute(), momentoInicio.second());
-    let fechaFin=new Date(momentoFin.year(), momentoFin.month(), momentoFin.day(), momentoFin.hour(), momentoFin.minute(), momentoFin.second());
-    eventos.push({
-      _id: e.idHor,
-      name: e.tipo,
-      startDateTime: fechaIni,
-      endDateTime: fechaFin,
-      classes: 'color-1'
-    })
-
-    console.log(fechaIni+" "+fechaFin)
-  })
-}
-
-// Y lo establezco como el estado
-this.setState({
-  eventos: eventos
-})
   }
 
   render() {
-    console.log(JSON.stringify(this.state.eventos))
 
-    const detalleEvento=(props)=>{
+    const detalleEvento = (props) => {
       return (<p>{props.item.name}</p>)
     }
 
@@ -119,6 +137,8 @@ this.setState({
         <ReactAgenda
           minDate={now}
           maxDate={new Date(now.getFullYear(), now.getMonth() + 3)}
+          startAtTime={1}
+          endAtTime={24}
           disablePrevButton={false}
           startDate={this.state.startDate}
           cellHeight={this.state.cellHeight}
@@ -126,10 +146,10 @@ this.setState({
           items={this.state.eventos}
           numberOfDays={this.state.numberOfDays}
           rowsPerHour={this.state.rowsPerHour}
-          itemColors={colors}
+          itemColors={colorEvento}
           itemComponent={detalleEvento}
           autoScale={false}
-          helper={true}
+          helper={false}
           fixedHeader={false}
           onRangeSelection={this.seleccionarRango}
           onChangeEvent={this.modificarEvento}
@@ -144,8 +164,8 @@ this.setState({
             ?
             <Modal clickOutside={this.cerrarModal} >
               <div className="modal-content">
-                <ReactAgendaCtrl items={this.state.items} itemColors={colors}
-                  selectedCells={this.state.selected} Addnew={this.addNewEvent} edit={this.editEvent} />
+                <ReactAgendaCtrl items={this.state.items} itemColors={colorSeleccionado}
+                  selectedCells={this.state.selected} Addnew={this.agregarEvento} edit={this.editEvent} />
 
               </div>
             </Modal> : ''
