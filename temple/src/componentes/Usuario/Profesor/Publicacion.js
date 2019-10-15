dc554d369f82fe7548e3a0a3f0044c0f0ea9c24d
gricardov <gricardov@gmail.com>
2019-10-15 01:15:57 -0500

    inicio profesor con publicaciones menos publicador

--- temple/src/componentes/Usuario/Profesor/Publicacion.js
+++ temple/src/componentes/Usuario/Profesor/Publicacion.js
@@ -4,7 +4,31 @@ import './Publicacion.css';
 const Publicacion = (props) => {
     return (
         <div className="tarjeta-publicacion">
-            adf
+            <img alt="img-usuario" src={props.datos.img} />
+            <div className="encabezado">
+                <h2>{props.datos.titulo}</h2>
+                <p>Publicado {props.datos.fecPub} por {props.datos.autor}</p>
+            </div>
+            <div className="contenido">
+                {
+                    props.datos.imgdes
+                        ?
+                        <img alt="img-descripcion" src={props.datos.imgdes} />
+                        :
+                        null
+                }
+                <div>
+                    <p>
+                        {props.datos.descripcion}
+                    </p>
+                </div>
+            </div>
+            <div className="estado">
+                <div>{props.datos.numCor} <i className="fa fa-heart"></i></div>
+                <div>{props.datos.numCom} <i className="fa fa-comment"></i></div>
+                <div>{props.datos.porcAmor} <i className="fa fa-eye"></i></div>
+
+            </div>
         </div>
     )
 
