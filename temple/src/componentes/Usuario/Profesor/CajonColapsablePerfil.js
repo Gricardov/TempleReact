55c5275ffa5aa688b05e193977df710f54a7e008
gricardov <gricardov@gmail.com>
2019-12-09 01:04:32 -0500

    embellecimiento de barra lateral colapsable

--- temple/src/componentes/Usuario/Profesor/CajonColapsablePerfil.js
+++ temple/src/componentes/Usuario/Profesor/CajonColapsablePerfil.js
@@ -14,30 +14,49 @@ class Cajon extends Component {
         let perfil = this.props.perfil;
 
         return (
-                <>
-                    {this.props.revisandoEjercicio ? <p className="txt-preguntado-por">Preguntado por: </p> : null}
-                    <div className="foto-perfil" style={{ backgroundImage: `url(${perfil.imgPer})` }}>
-                    </div>
-                    <h1 style={{ display: "block" }} className="nombres-perfil">{perfil.nomUsu} {perfil.apaUsu} {perfil.amaUsu}</h1>
-                    <p className="nombre-seguimiento">@{perfil.logUsu}</p>
+            <>
+                {this.props.revisandoEjercicio ? <p className="txt-preguntado-por">Preguntado por: </p> : null}
+                <div className="foto-perfil" style={{ backgroundImage: `url(${perfil.imgPer})` }}>
+                </div>
+                <h1 style={{ display: "block" }} className="nombres-perfil">{perfil.nomUsu} {perfil.apaUsu} {perfil.amaUsu}</h1>
+                <p className="nombre-seguimiento">@{perfil.logUsu}</p>
+                {!this.props.revisandoEjercicio
+                    ?
+                    <p className="profesion-usuario">Diseñador UX/UI</p>
+                    :
+                    null
+                }
+                {!this.props.revisandoEjercicio ?
+                    <>
 
-                    {!this.props.revisandoEjercicio ?
-                        <>
-                            <div className="contenedor-datos-resumidos-perfil">
-                                <i className="fa fa-lightbulb-o fa-2x" style={{ color: "gray" }}></i> Diseñador UI/UX<br />
-                                <i className="fa fa-suitcase fa-2x" style={{ color: "gray" }}></i> Academias Trilce
-                    </div>
-                            <hr />
-                            <div className="contenedor-atributos-perfil">
-                                <i className="fa fa-check-circle fa-2x" style={{ color: "green" }}></i> Perfil verificado<br />
-                                <i className="fa fa-check-circle fa-2x" style={{ color: "green" }}></i> 10 horas de experiencia<br />
-                                <i className="fa fa-check-circle fa-2x" style={{ color: "green" }}></i> Rango experto<br />
+                        <div className="contenedor-datos">
+                            <div className="icono-circulo verde sombra-lateral">
+                                <i className="fa fa-check" style={{ color: "white" }}></i>
                             </div>
-                        </>
-                        :
-                        null
-                    }
-                </>
+                            <p>Datos verificados</p>
+                            <div className="icono-circulo marron sombra-lateral">
+                                <i className="fa fa-briefcase" style={{ color: "white" }}></i>
+                            </div>
+                            <p>Academia IngNova</p>
+                            <div className="icono-circulo rojizo sombra-lateral">
+                                <i className="fa fa-university" style={{ color: "white" }}></i>
+                            </div>
+                            <p>45 horas dictadas</p>
+                            <div className="icono-circulo azulado sombra-lateral">
+                                <i className="fa fa-question" style={{ color: "white" }}></i>
+                            </div>
+                            <p>0 preguntas resueltas</p>
+                            <div className="icono-circulo gris sombra-lateral">
+                                <i className="fa fa-mortar-board" style={{ color: "white" }}></i>
+                            </div>
+                            <p>Rango Experto</p>
+                        </div>
+
+                    </>
+                    :
+                    null
+                }
+            </>
 
         )
     }
